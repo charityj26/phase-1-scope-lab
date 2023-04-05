0d337cd6d4c8b9c91100e6c212b3c001f5a0ff55
charity <charity.joseph@students.moringaschool.com>
2023-04-05 13:56:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+//write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "charity";
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+  console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "liz"
+}
